988a7cf778c9ea0acebe5425d61c18e1d15f9a5b
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-08-26 13:52:02 +0100

    handle add to cart: define react context, define cart items,create store provider and handle add to cart action for button

---
+++ utils/Store.js
@@ -0,0 +1,37 @@
+// this is to make cart-items available throughout the project.
+import { createContext, useReducer } from "react";
+
+// defining react context
+export const Store = createContext();
+
+// defining cart items state
+const initialState = {
+  cart: { cartItems: [] },
+};
+
+// defining reducer function.
+function reducer(state, action) {
+  switch (action.type) {
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item.slug === newItem.slug
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.name === existItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    default:
+      return state;
+  }
+}
+
+// create store provider
+export function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
+}
